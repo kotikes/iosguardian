6313e098b1fc5d122281ba0daf9e12f39e4441ea
Elizaveta <betti_hinata@mail.ru>
2026-03-09 19:07:22 +0300

    сделала чтобы видно было сайт только с телефона

---
+++ a/AppleCare Plus_files/common.js
@@ -0,0 +1,314 @@
+const translations = {
+  en: {
+    logoDesc: "Protection System",
+    settings: "Settings",
+    settingsItem1: "Name, Phone Numbers, Email",
+    settingsItem2: "Password & Security",
+    settingsItem3: "Payment & Shipping",
+    settingsItem4: "Subscriptions",
+    compromised: "Compromised",
+    appstoreName: "Media & Purchases",
+    locatorName: "Find My",
+    phone: "This iPhone",
+    action: "Repair now",
+    alertTitle: "Your Apple iPhone has been hacked",
+    alertSubtitle: "Your data has appeared in a data leak, which puts this device at high risk of compromise. Proceed to the App Store",
+    alertAction1: "Settings",
+    alertAction2: "OK",
+  },
+  fr: {
+    logoDesc: "Système de Protection",
+    settings: "Paramètres",
+    settingsItem1: "Nom, Numéros de Téléphone, E-mail",
+    settingsItem2: "Mot de passe et Sécurité",
+    settingsItem3: "Paiement et Livraison",
+    settingsItem4: "Abonnements",
+    compromised: "Compromis",
+    appstoreName: "Médias et Achats",
+    locatorName: "Trouver mon",
+    phone: "Cet iPhone",
+    action: "Réparer maintenant",
+    alertTitle: "Votre Apple iPhone a été piraté",
+    alertSubtitle: "Vos données sont apparues dans une fuite de données, ce qui met cet appareil en danger élevé de compromission. Accédez à l'App Store",
+    alertAction1: "Paramètres",
+    alertAction2: "OK"
+  },
+  es: {
+    logoDesc: "Sistema de Protección",
+    settings: "Configuración",
+    settingsItem1: "Nombre, Números de Teléfono, Correo Electrónico",
+    settingsItem2: "Contraseña y Seguridad",
+    settingsItem3: "Pago y Envío",
+    settingsItem4: "Suscripciones",
+    compromised: "Comprometido",
+    appstoreName: "Medios y Compras",
+    locatorName: "Encuentra mi",
+    phone: "Este iPhone",
+    action: "Reparar ahora",
+    alertTitle: "Su iPhone de Apple ha sido hackeado",
+    alertSubtitle: "Sus datos han aparecido en una filtración de datos, lo que pone este dispositivo en alto riesgo de compromiso. Vaya a la App Store",
+    alertAction1: "Configuración",
+    alertAction2: "OK"
+  },
+  de: {
+    logoDesc: "Schutzsystem",
+    settings: "Einstellungen",
+    settingsItem1: "Name, Telefonnummern, E-Mail",
+    settingsItem2: "Passwort und Sicherheit",
+    settingsItem3: "Zahlung und Versand",
+    settingsItem4: "Abonnements",
+    compromised: "Kompromittiert",
+    appstoreName: "Medien & Einkäufe",
+    locatorName: "Mein iPhone suchen",
+    phone: "Dieses iPhone",
+    action: "Jetzt reparieren",
+    alertTitle: "Ihr Apple iPhone wurde gehackt",
+    alertSubtitle: "Ihre Daten sind in einem Datenleck aufgetaucht, was dieses Gerät in erhebliche Gefahr bringt. Gehen Sie zum App Store",
+    alertAction1: "Einstellungen",
+    alertAction2: "OK"
+  },
+  pt: {
+    logoDesc: "Sistema de Proteção",
+    settings: "Configurações",
+    settingsItem1: "Nome, Números de Telefone, E-mail",
+    settingsItem2: "Senha e Segurança",
+    settingsItem3: "Pagamento e Envio",
+    settingsItem4: "Assinaturas",
+    compromised: "Comprometido",
+    appstoreName: "Mídia e Compras",
+    locatorName: "Encontrar Meu",
+    phone: "Este iPhone",
+    action: "Reparar agora",
+    alertTitle: "Seu iPhone Apple foi hackeado",
+    alertSubtitle: "Seus dados apareceram em um vazamento de dados, o que coloca este dispositivo em alto risco de comprometimento. Vá para a App Store",
+    alertAction1: "Configurações",
+    alertAction2: "OK"
+  },
+  ar: {
+    logoDesc: "نظام الحماية",
+    settings: "الإعدادات",
+    settingsItem1: "الاسم، أرقام الهاتف، البريد الإلكتروني",
+    settingsItem2: "كلمة المرور والأمان",
+    settingsItem3: "الدفع والشحن",
+    settingsItem4: "الاشتراكات",
+    compromised: "تم الاختراق",
+    appstoreName: "الوسائط والمشتريات",
+    locatorName: "البحث عن هاتفي",
+    phone: "هذا الآيفون",
+    action: "إصلاح الآن",
+    alertTitle: "تم اختراق جهاز iPhone الخاص بك",
+    alertSubtitle: "بياناتك ظهرت في تسريب بيانات، مما يضع هذا الجهاز في خطر عالي من التعرض للاختراق. انتقل إلى متجر التطبيقات",
+    alertAction1: "الإعدادات",
+    alertAction2: "موافق"
+  },
+  ja: {
+    logoDesc: "保護システム",
+    settings: "設定",
+    settingsItem1: "名前、電話番号、メール",
+    settingsItem2: "パスワードとセキュリティ",
+    settingsItem3: "支払いと発送",
+    settingsItem4: "サブスクリプション",
+    compromised: "侵害されました",
+    appstoreName: "メディアと購入",
+    locatorName: "私のを探す",
+    phone: "このiPhone",
+    action: "今すぐ修理",
+    alertTitle: "Apple iPhoneがハッキングされました",
+    alertSubtitle: "あなたのデータがデータリークに表示されました、これによりこのデバイスは高い危険にさらされました。App Storeに移動してください",
+    alertAction1: "設定",
+    alertAction2: "OK"
+  },
+  ko: {
+    logoDesc: "보호 시스템",
+    settings: "설정",
+    settingsItem1: "이름, 전화 번호, 이메일",
+    settingsItem2: "암호 및 보안",
+    settingsItem3: "결제 및 배송",
+    settingsItem4: "구독",
+    compromised: "침해당함",
+    appstoreName: "미디어 및 구매",
+    locatorName: "내를 찾아보세요",
+    phone: "이 iPhone",
+    action: "지금 고치기",
+    alertTitle: "Apple iPhone이 해킹되었습니다",
+    alertSubtitle: "귀하의 데이터가 데이터 유출에 표시되어이 장치가 높은 위험에 노출되었습니다. App Store로 이동하십시오",
+    alertAction1: "설정",
+    alertAction2: "확인"
+  },
+  tr: {
+    logoDesc: "Koruma Sistemi",
+    settings: "Ayarlar",
+    settingsItem1: "Ad, Telefon Numaraları, E-posta",
+    settingsItem2: "Şifre ve Güvenlik",
+    settingsItem3: "Ödeme ve Kargo",
+    settingsItem4: "Abonelikler",
+    compromised: "Tehdit Altında",
+    appstoreName: "Medya ve Alışveriş",
+    locatorName: "Benim Cihazımı Bul",
+    phone: "Bu iPhone",
+    action: "Şimdi Onar",
+    alertTitle: "Apple iPhone'unuz hacklendi",
+    alertSubtitle: "Verileriniz bir veri sızıntısında göründü, bu da bu cihazı yüksek risk altına sokuyor. App Store'a gidin",
+    alertAction1: "Ayarlar",
+    alertAction2: "Tamam"
+  },
+  it: {
+    logoDesc: "Sistema di Protezione",
+    settings: "Impostazioni",
+    settingsItem1: "Nome, Numeri di Telefono, Email",
+    settingsItem2: "Password e Sicurezza",
+    settingsItem3: "Pagamento e Spedizione",
+    settingsItem4: "Abbonamenti",
+    compromised: "Compromesso",
+    appstoreName: "Media e Acquisti",
+    locatorName: "Trova il Mio",
+    phone: "Questo iPhone",
+    action: "Ripara ora",
+    alertTitle: "Il tuo iPhone Apple è stato hackerato",
+    alertSubtitle: "I tuoi dati sono comparsi in una fuga di dati, mettendo questo dispositivo a rischio elevato di compromissione. Vai all'App Store",
+    alertAction1: "Impostazioni",
+    alertAction2: "OK"
+  },
+  nl: {
+    logoDesc: "Beveiligingssysteem",
+    settings: "Instellingen",
+    settingsItem1: "Naam, Telefoonnummers, E-mail",
+    settingsItem2: "Wachtwoord en Beveiliging",
+    settingsItem3: "Betaling en Verzending",
+    settingsItem4: "Abonnementen",
+    compromised: "Gecompromitteerd",
+    appstoreName: "Media & Aankopen",
+    locatorName: "Zoek Mijn",
+    phone: "Deze iPhone",
+    action: "Nu repareren",
+    alertTitle: "Uw Apple iPhone is gehackt",
+    alertSubtitle: "Uw gegevens zijn verschenen in een datalek, wat dit apparaat een hoog risico op compromis oplevert. Ga naar de App Store",
+    alertAction1: "Instellingen",
+    alertAction2: "OK"
+  },
+  el: {
+    logoDesc: "Σύστημα Προστασίας",
+    settings: "Ρυθμίσεις",
+    settingsItem1: "Όνομα, Τηλεφωνικοί Αριθμοί, Email",
+    settingsItem2: "Κωδικός & Ασφάλεια",
+    settingsItem3: "Πληρωμές & Αποστολή",
+    settingsItem4: "Συνδρομές",
+    compromised: "Κινδυνευμένο",
+    appstoreName: "Μέσα & Αγορές",
+    locatorName: "Βρες τη Μονάδα",
+    phone: "Αυτό το iPhone",
+    action: "Επισκευή τώρα",
+    alertTitle: "Το Apple iPhone σας έχει διαρρεύσει",
+    alertSubtitle: "Τα δεδομένα σας εμφανίστηκαν σε διαρροή δεδομένων, κάτι που θέτει αυτή τη συσκευή σε υψηλό κίνδυνο διακινδύνευσης. Μεταβείτε στο App Store",
+    alertAction1: "Ρυθμίσεις",
+    alertAction2: "OK"
+  },
+  pl: {
+    logoDesc: "System Ochrony",
+    settings: "Ustawienia",
+    settingsItem1: "Imię, Numery Telefonów, Email",
+    settingsItem2: "Hasło i Bezpieczeństwo",
+    settingsItem3: "Płatność i Dostawa",
+    settingsItem4: "Subskrypcje",
+    compromised: "Naruszenie",
+    appstoreName: "Media i Zakupy",
+    locatorName: "Znajdź Moje",
+    phone: "To iPhone",
+    action: "Napraw teraz",
+    alertTitle: "Twój iPhone Apple został zhackowany",
+    alertSubtitle: "Twoje dane pojawiły się w wycieku danych, co stawia to urządzenie w wysokim ryzyku naruszenia. Przejdź do App Store",
+    alertAction1: "Ustawienia",
+    alertAction2: "OK"
+  }
+}
+
+const definedLanguages = ["en", "fr", "es", "de", "pt", "ar", "ja", "ko", "tr", "it", "nl", "el", "pl"];
+
+const currentLocale = (navigator && navigator.language) || "en";
+
+const getLocale = () => {
+  const first2FromLocaleStr = currentLocale.slice(0, 2);
+  if (definedLanguages.includes(first2FromLocaleStr)) {
+    return first2FromLocaleStr;
+  }
+
+  return "en";
+}
+
+const locale = getLocale();
+
+const localeTranslations = translations[locale];
+
+const logoDescEl = document.getElementById("logoDesc");
+logoDescEl.innerHTML = localeTranslations.logoDesc;
+
+const settingsEl = document.getElementById("settings");
+settingsEl.innerHTML = localeTranslations.settings;
+
+const settingsItem1El = document.getElementById("settingsItem1");
+settingsItem1El.innerHTML = localeTranslations.settingsItem1;
+
+const settingsItem2El = document.getElementById("settingsItem2");
+settingsItem2El.innerHTML = localeTranslations.settingsItem2;
+
+const settingsItem3El = document.getElementById("settingsItem3");
+settingsItem3El.innerHTML = localeTranslations.settingsItem3;
+
+const settingsItem4El = document.getElementById("settingsItem4");
+settingsItem4El.innerHTML = localeTranslations.settingsItem4;
+
+const iCloudCompromisedEl = document.getElementById("iCloudCompromised");
+iCloudCompromisedEl.innerHTML = localeTranslations.compromised;
+
+const appstoreNameEl = document.getElementById("appstoreName");
+appstoreNameEl.innerHTML = localeTranslations.appstoreName;
+
+const appstoreCompromisedEl = document.getElementById("appstoreCompromised");
+appstoreCompromisedEl.innerHTML = localeTranslations.compromised;
+
+const locatorNameEl = document.getElementById("locatorName");
+locatorNameEl.innerHTML = localeTranslations.locatorName;
+
+const locatorCompromisedEl = document.getElementById("locatorCompromised");
+locatorCompromisedEl.innerHTML = localeTranslations.compromised;
+
+const phoneEl = document.getElementById("phone");
+phoneEl.innerHTML = localeTranslations.phone;
+
+const actionEl = document.getElementById("action");
+actionEl.innerHTML = localeTranslations.action;
+
+const alertTitleEl = document.getElementById("alertTitle");
+alertTitleEl.innerHTML = localeTranslations.alertTitle;
+
+const alertSubtitleEl = document.getElementById("alertSubtitle");
+alertSubtitleEl.innerHTML = localeTranslations.alertSubtitle;
+
+const alertAction1El = document.getElementById("alertAction1");
+alertAction1El.innerHTML = localeTranslations.alertAction1;
+
+const alertAction2El = document.getElementById("alertAction2");
+alertAction2El.innerHTML = localeTranslations.alertAction2;
+
+var now_seconds = 90,
+  timer = document.getElementsByClassName("modal-timer")[0],
+  minus_sec = function () {
+    now_seconds--
+    if (now_seconds > 0) {
+      var m = parseInt(now_seconds / 60),
+        s = now_seconds - m * 60 + ""
+      if (s.length <= 1) s = "0" + s
+      timer.innerHTML = m + ":" + s
+      setTimeout(minus_sec, 1000)
+    } else {
+      timer.innerHTML = "0:00"
+    }
+  }
+minus_sec()
+
+var modal = document.getElementsByClassName("modal")[0],
+  btns = document.getElementsByClassName("modal-btn")
+for (var btn of btns) btn.addEventListener("click", function (e) {
+  e.preventDefault()
+  modal.style.display = "none"
+})
